d9a79e06d0a901a8b8d9a113f320b0d3dd08ca1e
Jackson <HiEvgenij@yandex.ru>
2022-07-24 00:22:56 +0300

    запустил api ингредиентов

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,1 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import IngredientViewSet
+
+
+router = SimpleRouter()
+router.register('ingredients', IngredientViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Ingredients
+from .serializers import IngredientsSerializer
 
-# Create your views here.
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredients.objects.all()
+    serializer_class = IngredientsSerializer
