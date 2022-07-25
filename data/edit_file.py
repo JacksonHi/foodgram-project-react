5cf35a7a6516d36eebbb45debf3c84f41eaef897
jacksonHi <argentina43@yandex.ru>
2022-07-25 18:54:01 +0300

    xxx

---
+++ data/edit_file.py
@@ -0,0 +1,21 @@
+import json
+
+"Редактирует файл с ингредиентами"
+with open('data/ingredients.json', 'r', encoding='utf-8') as f:
+    text = json.load(f)
+    pk = 0
+    list = []
+    
+    for i in text:
+        data = {
+              "model":  "api.ingredients",
+              "pk":  pk,
+              "fields":  {}
+           }
+        data['pk'] = pk
+        data['fields']['name'] = i['name']
+        data['fields']['measurement_unit'] = i['measurement_unit']
+        list.append(data)
+        pk += 1
+    with open('data/new_ingredients.json', 'w') as write_file:
+        json.dump(list, write_file)
--- data/load_fixtures.py
+++ data/load_fixtures.py
@@ -1,22 +1,9 @@
-import json, os
+import os, platform
 
 
-with open('data/ingredients.json', 'r', encoding='utf-8') as f:
-    text = json.load(f)
-    pk = 0
-    list = []
-    
-    for i in text:
-        data = {
-              "model":  "api.ingredients",
-              "pk":  pk,
-              "fields":  {}
-           }
-        data['pk'] = pk
-        data['fields']['name'] = i['name']
-        data['fields']['measurement_unit'] = i['measurement_unit']
-        list.append(data)
-        pk += 1
-    with open('data/new_ingredients.json', 'w') as write_file:
-        json.dump(list, write_file)
-os.system('py backend/foodgram/manage.py loaddata new_ingredients.json')
+# загружает фикстуры в базу
+system = platform.system()
+if system == 'Windows':
+    os.system('py backend/foodgram/manage.py loaddata new_ingredients.json tag.json')
+elif system == 'Linux':
+    os.system('python backend/foodgram/manage.py loaddata new_ingredients.json tag.json')
