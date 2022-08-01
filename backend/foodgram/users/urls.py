e8afb636832d95dac8f280e25a6af36bc07cc44e
jacksonHi <argentina43@yandex.ru>
2022-08-01 18:47:28 +0300

    mmm

--- backend/foodgram/users/urls.py
+++ backend/foodgram/users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import FollowViewSet
 
 
 app_name = 'urls'
 
+router = SimpleRouter()
+router.register('subscribe', FollowViewSet)
+
 urlpatterns = [
+    path('', include(router.register)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
 ]
--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -6,3 +6,11 @@ from backend.foodgram.users.serializers import CastomUserSerializer
 class CastomUserViewSet(UserViewSet):
     serializer_class = CastomUserSerializer
 
+    def subscribe():
+        pass
+
+    def unsubscribe():
+        pass
+
+    def subscriptions():
+        pass
